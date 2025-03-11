8c823b464f9fe7749a82780cf2ffc7b464069aec
Vilander Costa <110786239+Vilander@users.noreply.github.com>
2025-03-11 13:56:25 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
@@ -7,14 +8,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="vilander",
+    author_email="vilander.costa@gmail.com",
+    description="aula DIO",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/Vilander/simple-package-template",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
